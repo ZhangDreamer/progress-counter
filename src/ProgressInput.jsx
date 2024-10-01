d5875492e91a86f3b1a60cc4d506b34c69dec6ed
Anyel <anyelzhang@gmail.com>
2024-09-30 22:09:43 -0500

    add initial html for progressInput

--- src/ProgressInput.jsx
+++ src/ProgressInput.jsx
@@ -3,7 +3,12 @@ function ProgressInput(){
 
   return(
     <div className="progress-input-container">
-      
+      <label for='win'>Win</label>
+      <input type="radio" name='option' id='win' value="win"/>
+      <label for='Lose'>Lose</label>
+      <input type="radio" name='option' id='lose' value="lose"/>
+      <input type="text"/>
+      <button>▶</button>
     </div>
   )
 }
