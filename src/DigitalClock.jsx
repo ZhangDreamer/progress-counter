a0ef9250dc4e0d627d2373dcdbceeaa3ff7c0d0c
Anyel <anyelzhang@gmail.com>
2024-10-17 23:31:45 -0500

    Refactor DigitalClock component to fetch time from API and update state with timezone

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,33 +1,50 @@
 import React, {useEffect, useState} from 'react';
+import fetchTime from './backend/fetchTime.js';
 
 function DigitalClock(){
 
-  const [time, setTime] = useState(new Date());
+  const [time, setTime] = useState(null);
+  const [timezone, setTimezone] = useState(null);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-
-    return () => {
-      clearInterval(intervalId);
+    const getTime = async () => {
+      const fetchedTime = await fetchTime();
+      setTime(new Date(fetchedTime.datetime));
+      setTimezone(fetchedTime.abbreviation);
     }
+
+    getTime();
   }, []);
 
+  useEffect(() => {
+    if (time) {
+      const interval = setInterval(() => {
+        setTime((prevTime) => new Date(prevTime.getTime() + 1000));
+      }, 1000);
+      
+      return () => clearInterval(interval); 
+    }
+  }, [time]);
+
+
   function formatTime(){
-    let hours = time.getHours();
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
-    const meridiem = hours >= 12 ? "PM" : "AM";
-
-    hours = hours % 12 || 12;
-
-    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem} EST`;
+    if(time){
+      let hours = time.getHours();
+      const minutes = time.getMinutes();
+      const seconds = time.getSeconds();
+      const meridiem = hours >= 12 ? "PM" : "AM";
+  
+      hours = hours % 12 || 12;
+      return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem} ${timezone}`;
+    } else {
+      return ''
+    }
   }
 
   function padZero(number){
     return (number < 10 ? "0" : "") + number;
   }
+
 
   return(
   <div>
