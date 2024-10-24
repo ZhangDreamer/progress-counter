b6d3b5af7f1f9ceda2dc343431d5101cf2d8357a
Anyel <anyelzhang@gmail.com>
2024-10-24 00:55:04 -0500

    remove unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <LocationContext.Provider value="America/Panama">
-      <ProgressDisplay value="America/Panama"/>
+      <ProgressDisplay/>
     </LocationContext.Provider>
     </>
   )
