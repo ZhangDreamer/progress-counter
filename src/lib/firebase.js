9dd9accdba30830c1c27030d28fba8c43e6c0cf9
Anyel <anyelzhang@gmail.com>
2024-11-02 22:47:15 -0500

    get firestore database

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp} from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,10 +12,14 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  databaseURL:import.meta.env.VITE_APP_FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
 
 export default app
